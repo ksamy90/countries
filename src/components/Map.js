c74b1e50730791e7179003450b78878398243842
samson <samson.omondi1@ibm.com>
2023-04-16 14:54:03 +0300

    add specific maps for each country

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,11 @@ import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 const Map = ({ position }) => {
   return (
-    <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
+    <MapContainer
+      center={[position[0], position[1]]}
+      zoom={12}
+      scrollWheelZoom={false}
+    >
       <Marker position={[position[1], position[0]]} />
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
